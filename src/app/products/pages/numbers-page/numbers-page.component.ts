1944f5940d8d51e434e301da49962be8e9211756
Leamsi Alvarado <leamsi-a-alvarado-g@outlook.com>
2024-06-17 15:36:10 -0600

    starts and ends with number pipes and starts with uncommon pipes

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
   styleUrl: './numbers-page.component.css'
 })
-export class NumbersPageComponent {
+export class NumbersPageComponent{
+
+  public totalSells: number = 5191971.1655;
+  public percent: number = .04852
 
 }
